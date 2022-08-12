4303a11cb4f0cdb823dae494eb1c3872ac624da9
KLZ <kevin.lagos.z@gmail.com>
2022-08-12 04:20:33 -0400

    agrego funcion de obtener ip`s locales

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import conf
 import requests
 import json
 
+sandbox = "https://10.10.20.14"
 def obtener_token(usuario, clave):
     url = "https://10.10.20.14//api/aaaLogin.json"
     body = {
@@ -20,6 +21,20 @@ def obtener_token(usuario, clave):
     token = respuesta.json()["imdata"][0]["aaaLogin"]["attributes"]["token"]
     return token
 
-token = obtener_token(conf.usuario, conf.clave)
+# GET http://apic-ip-address/api/class/topSystem.json
 
-print(token)
+def top_system():
+    cabecera = {
+        "Content-Type": "application/json"
+    }
+    galleta = {
+        "APIC-Cookie": obtener_token(conf.usuario, conf.clave)
+    }
+    requests.packages.urllib3.disable_warnings()
+    respuesta = requests.get(sandbox+"/api/class/topSystem.json", headers=cabecera, cookies= galleta, verify=False)
+    for i in range(0, 4):
+        ip_local = respuesta.json()["imdata"][i]["topSystem"]["attributes"]["address"]
+        print(ip_local)
+
+
+top_system()
